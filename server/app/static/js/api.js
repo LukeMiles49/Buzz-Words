3585d24326cfbb9fb8605e9e8688b7b4be609659
Luke Miles <luke.49@live.co.uk>
2021-02-27 22:38:31 +0000

    API interface

---
+++ server/app/static/js/api.js
@@ -0,0 +1,25 @@
+let myId;
+
+async function requestHistorySince(index) {
+	return [
+		"Paragraph 1",
+		"Paragraph 2"
+	];
+}
+
+async function requestOptions() {
+	// TODO
+	
+	const index = 3;
+	const options = [
+		"Option 1",
+		"Option 2",
+		"Option 3"
+	];
+	
+	return [index, options];
+}
+
+async function castVote(optionIndex) {
+	// TODO
+}
